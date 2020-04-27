653e89b782fd20f96af3db8ccf930bf99a68ca6a
gillie1022 <dave.gillie@icloud.com>
2020-04-27 11:50:58 -0400

    need to go over functions

--- homework.js
+++ homework.js
@@ -1,10 +1,18 @@
 // 1. Set the variable `givenName` to the string "Addison".
 
+let givenName = "Addison"
+
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
 
+let candies = 20
+let people = 6
+let leftover = 20 % 6
+
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
+
+
 
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
